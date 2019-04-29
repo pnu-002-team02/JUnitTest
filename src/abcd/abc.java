5f323561dce35784585004277d6453d1e857c23e
operatos <operato@naver.com>
2019-04-29 18:56:10 +0900

    first

---
+++ src/abcd/abc.java
@@ -0,0 +1,7 @@
+package abcd;
+
+public class abc {
+	public double add(double number1, double number2) {
+		return number1 + number2;
+	}
+}
---
+++ src/abcd/abcTest.java
@@ -0,0 +1,14 @@
+package abcd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class abcTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
